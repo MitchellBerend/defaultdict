84eac014513a1373fc144154a545977ae94fbfdc
Mitchell Berendhuysen <45570310+MitchellBerend@users.noreply.github.com>
2023-03-12 13:06:46 +0100

    Update docs (#6)

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! This library exposes a struct that mimicks the behaviour of the python
+//! This library exposes structs that mimicks the behaviour of the python
 //! [defaultdict](https://docs.python.org/3/library/collections.html#collections.defaultdict).
 //!
 //! This behaviour does require that the type of the value does have the [`Default`] implemented.
